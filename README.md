39aa4587bf74377959be1ca7d899ff73f331bfe2
Kevin Logan <kmlo225@cs.uky.edu>
2016-09-14 10:15:57 -0400

    Basic index.php and README

---
+++ README.md
@@ -0,0 +1,6 @@
+CS 405 Intro to Databases: Program
+Kevin Logan
+Ryan Ludt
+Rohan Ganesan
+
+
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>CS405 Program</title>
+</head>
+<body>
+
+<h1>We are the dream team</h1>
+
+<p>My first paragraph.</p>
+
+</body>
+</html>
