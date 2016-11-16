d3de284355bd2a6d39b2fc9d667eed71a5c58b46
trludt <trludt@gmail.com>
2016-11-16 17:50:37 -0500

    Finished Add to Cart

--- addToCart.php
+++ addToCart.php
@@ -1,6 +1,7 @@
 <?php
 	$ProductID = $_POST['ProductID'];
 	echo $ProductID;
+	echo "<br>";	
 
 	$logged_in = False;
 	if (isset($_COOKIE['login'])) {
@@ -20,20 +21,86 @@
         	}
 
 		$sql = "select * 
-			from orders, userOrder 
-			where OrderStatus='UnCheckout'
-			and UserID=" . $_COOKIE['login'] . ";";
-        	echo $sql;
+			from orders, userOrder
+			where OrderStatus = 'UnCheckout'
+			and orders.OrderID = userOrder.OrderID
+			and UserID=" . $_COOKIE['login'] . ";"; 
 
 		$result = $conn->query($sql);
+		$data = $result->fetch_assoc();
+                $orderID = $data["OrderID"];
+		echo "<br><br> $orderID";
 
+		// If there is an order with OrderStatus='UnCheckout' connected to the UserID of the person logged in
 		if ($result->num_rows > 0) {
-			echo "<br> There are items in the cart";
-			//add product
+			echo "<br> We do have an order with status UnCheckout connected to this UserID";
+			// This means the item we are adding to the cart is already in the cart, and we need to add another one of these items to the cart
+			// as long as there are still enough quanity left? (not sure about quantity thing)
+			
+			$sql1 = "select o.OrderID 
+                        	from orderContainProduct as o, orders, userOrder 
+                        	where orders.OrderStatus='UnCheckout'
+                        	and o.OrderID = orders.OrderID
+                        	and o.OrderID = userOrder.OrderID
+                        	and o.ProductID = ". $ProductID ."
+                        	and UserID=" . $_COOKIE['login'] . ";";
+
+			$result = $conn->query($sql1);
+
+			if ($result->num_rows > 0) {
+				echo "<br> This item is in the cart already. Adding it again";
+				
+				$sql2 = "update orderContainProduct
+					 set Amount=(Amount+1)
+					 where OrderID = ". $orderID ."
+					 and ProductID = ". $ProductID .";";
+					
+				$conn->query($sql2);
+				
+				session_start();
+                                $_SESSION['alert'] = "The product has been added to your cart!";
+				header('location:index.php'); // take user back to home page after product is added to cart
+			}
+			else {
+				echo "<br> This item is NOT in the cart. Adding it for first time now!";
+			
+				// Insert the ProductID to orderContainProduct table with Amount = 1 for the same OrderID
+				$sql2 = "insert into orderContainProduct
+					 values(". $orderID .",". $ProductID .", 1);";
+				$conn->query($sql2);
+				
+				session_start();
+                        	$_SESSION['alert'] = "The product has been added to your cart!";
+				header('location:index.php'); // take user back to home page after product is added to cart
+			}			
+
 		}
 		else{	
 			//Insert a new OrderID with OrderStatus='UnCheckout' then add the ProductID to the orderContainProduct table with Amount=1 for that same OrderID.
-        	}
+			echo "<br> We do NOT have an order with status UnCheckout connected to this UserID";
+		
+			$sql1 = "select max(OrderID) from orders;";
+			$result = $conn->query($sql1);
+			$row = $result->fetch_assoc();
+			$orderID = $row["max(OrderID)"];
+			$orderID++;
+
+			$sql2 = "insert into orders(OrderID, OrderStatus)
+				 values(". $orderID .",'UnCheckout');";
+			$conn->query($sql2);
+
+			$sql3 = "insert into userOrder
+				 values(". $_COOKIE['login'] .",". $orderID .");";
+			$conn->query($sql3);
+
+			$sql4 = "insert into orderContainProduct
+				 values(". $orderID .",". $ProductID .", 1);";
+			$conn->query($sql4);
+			
+			session_start();
+                	$_SESSION['alert'] = "The product has been added to your cart!";
+			header('location:index.php'); // take user back to home page after product is added to cart			
+		}
 		// output data of each row
         	$conn->close();
 
--- index.php
+++ index.php
@@ -15,28 +15,9 @@
      <div id="header"></div>
      <h1>Welcome to the Home page</h1>
      <form action="demo_form.asp">
-       Enter your search: <input type="text" name="searchBar" placeholder="This doesn't do anything yet"><br>
-       <input type="submit" value="Submit">
+       &nbsp; Search for Product: &nbsp;&nbsp; <input type="text" name="searchBar" size="30px" placeholder="Enter the Name or Category"><br>
+       &nbsp; <input type="submit" value="Submit">
      </form>
-     <h2>Notes for Add product to Cart Setup:</h2>
-     <p>When the product page is set up you need to have a button called "Add to Cart" that which is the submit button of a form to<br> 
-	another php file. This php file will add the product to the cart. In order to do this you need to do something specific based off what<br>
-	I've done so far. DO<br>
-	IF(There is an order with OrderStatus='UnCheckout' connected to the UserID of the person logged in){<br>
-		&nbsp;If(The product is already there){<br>
-			&emsp;&emsp; Increase the amount<br>
-		&nbsp;}<br>
-		&nbsp;else{<br>
-			&emsp;&emsp; add the ProductID to the orderContainProduct table with Amount=1 for that same OrderID.<br>
-		&nbsp;}<br>
-	&nbsp;}<br>
-	&nbsp;Else{<br>
-		&emsp;&emsp;Insert a new OrderID with OrderStatus='UnCheckout'<br>
-		&emsp;&emsp;then add the ProductID to the orderContainProduct table with Amount=1 for that same OrderID.<br>
-		&emsp;&emsp;NOTE:For deciding the new OrderID I would do Max(OrderID) and just increment it.<br>
-	&nbsp;}<br>
-		
-		</p>
      <h2>Notes for checkout:</h2>
      <p>All the really needs to be done for this is that you need to update the OrderStatus to OrderStatus='Pending'. You also need to update<br>
 	the OrderDate. You can easily do OrderDate=CurDate() in the query.
