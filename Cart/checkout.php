a2fbcaa16aa624ca5568b654e2c5d575f7520058
Kevin Logan <kevlogan7@gmail.com>
2016-11-18 12:18:29 -0500

    Finish Checkout

--- Cart/checkout.php
+++ Cart/checkout.php
@@ -14,14 +14,12 @@
   <body>
      <div id="header"></div>
      <h1>Checkout</h1>
-     <form action="/CS405/Cart/procCheckout.php">
-        <input type="submit" value="Complete Checkout" />
-      </form>
      <h2>Order Information</h2>
      <table>
      <thead>
        <th>Product</th>
-       <th>Class</th>
+       <th>Category</th>
+       <th>Price</th>
        <th>Amount</th>
      </thead>
      <tbody>
@@ -35,26 +33,80 @@
         if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
         }
-
-        $sql = "select * from order where OrderID='" . $_COOKIE[$cookie_name] . "';";
-        echo $sql;
+	//Get sum of price and salesprice
+	$sql = "select SUM(product.Price) as Price, SUM(product.SalesPrice) as SalesPrice, SUM(InvoicePrice) as InvoiceTotal 
+                from orderContainProduct as o, userOrder, product, orders 
+                where o.OrderID=userOrder.OrderID 
+                and product.ProductID=o.ProductID 
+                and orders.OrderID=o.OrderID 
+                and orders.OrderStatus='UnCheckout'
+                and UserID=" . $_COOKIE['login'] . ";";
+        $result = $conn->query($sql);
+	$data = $result->fetch_assoc();
+	//If sales price sum is 0 print PriceSum otherwise add each individual product 
+	//together based on SalesPrice or Price
+	$InvoiceTotal = number_format($data["InvoiceTotal"], 2, '.', '');
+	if($data["SalesPrice"] == 0){
+		$OrderTotal = number_format($data["Price"], 2, '.', '');
+	}
+	else{
+		$sql = "select Price, SalesPrice, InvoicePrice 
+                from orderContainProduct as o, userOrder, product, orders 
+                where o.OrderID=userOrder.OrderID 
+                and product.ProductID=o.ProductID 
+                and orders.OrderID=o.OrderID 
+                and orders.OrderStatus='UnCheckout'
+                and UserID=" . $_COOKIE['login'] . ";";
+        	$result = $conn->query($sql);
+		$OrderTotal=0;
+		while($row = $result->fetch_assoc()) {
+			if($row["SalesPrice"] == 0){
+				$OrderTotal += $row["Price"];
+			}
+			else{
+				$OrderTotal += $row["SalesPrice"];
+			}
+	        }
+		$OrderTotal = number_format($OrderTotal, 2, '.', '');
+	}
+	//Print out the order total
+        echo "<p>OrderTotal: $OrderTotal</p>";
+	
+	 $sql = "select Price, ProductName, SalesPrice, o.OrderID, Category, o.Amount 
+                from orderContainProduct as o, userOrder, product, orders 
+                where o.OrderID=userOrder.OrderID 
+                and product.ProductID=o.ProductID 
+                and orders.OrderID=o.OrderID 
+                and orders.OrderStatus='UnCheckout'
+                and UserID=" . $_COOKIE['login'] . ";";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
         // output data of each row
-         while($row = $result->fetch_assoc()) {
+           while($row = $result->fetch_assoc()) {
                 echo "<tr>";
-                echo "<td>" . $row["Product"] . "</td>";
-                echo "<td>" . $row["Class"] . "</td>";
+                echo "<td>" . $row["ProductName"] . "</td>";
+                echo "<td>" . $row["Category"] . "</td>";
+                if($row["SalesPrice"] != 0){
+                  echo "<td>" . $row["SalesPrice"] . "</td>";
+                }
+                else {
+                  echo "<td>" . $row["Price"] . "</td>";
+                }
                 echo "<td>" . $row["Amount"] . "</td>";
                 echo "</tr>";
-        }
+		$OrderID = $row["OrderID"];
+          }
         } else {
            echo "<br>Your Cart is empty<br>";
         }
-
-        $conn->close();
+        
+	$conn->close();
      ?>
+     </tbody>
+     </table>
+
+     <!--Address Table-->
      <table>
      <thead>
        <th>Address</th>
@@ -62,37 +114,15 @@
        <th>Zip</th>
      </thead>
      <tbody>
-     <?php
-        include '../databaselogin.php';
+       <tr>
+         <td>320 East Maxwell St.</td>
+	 <td>Lexington</td>
+	 <td>KY</td>
+       </tr>
+     </tbody>
+     </table>     
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $db);
-
-        // Check connection
-        if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-        }
-
-        $sql = "select * from order where OrderID='" . $_COOKIE[$cookie_name] . "';";
-        echo $sql;
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-        // output data of each row
-         while($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row["Address"] . "</td>";
-                echo "<td>" . $row["City"] . "</td>";
-                echo "<td>" . $row["Zip"] . "</td>";
-                echo "</tr>";
-        }
-        } else {
-           echo "<br>Your Address isn't in the system<br>";
-        }
-
-        $conn->close();
-     ?>
-
+     <!--Card information table-->
      <table>
      <thead>
        <th>Card</th>
@@ -100,37 +130,18 @@
        <th>Expiration</th>
      </thead>
      <tbody>
-     <?php
-        include '../databaselogin.php';
-
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $db);
-
-        // Check connection
-        if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-        }
-
-        $sql = "select * from order where OrderID='" . $_COOKIE[$cookie_name] . "';";
-        echo $sql;
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-        // output data of each row
-         while($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row["Card"] . "</td>";
-                echo "<td>" . $row["Card Name"] . "</td>";
-                echo "<td>" . $row["Expiration"] . "</td>";
-                echo "</tr>";
-        }
-        } else {
-           echo "<br>Your Card isn't in the system<br>";
-        }
-
-        $conn->close();
-     ?>
+       <tr>
+	 <td>123456789</td>
+	 <td>John M Doe</td>
+	 <td>11/11/2020</td>
+       </tr>
+     </tbody>
+     </table>
+      <form action="/CS405/Cart/procCheckout.php" method='post'>
+	<input type="Hidden" name='OrderTotal' value=" <?php echo $OrderTotal ?> " />
+	<input type="Hidden" name='OrderID' value=" <?php echo $OrderID ?> " />
+	<input type="Hidden" name='InvoiceTotal' value=" <?php echo $InvoiceTotal ?> " />
+        <input type="submit" value="Complete Checkout" />
+      </form>
 </body>
 </html>
-
-
--- Cart/procCheckout.php
+++ Cart/procCheckout.php
@@ -3,21 +3,26 @@
 include '../databaselogin.php';
 $conn = new mysqli($servername, $username, $password, $db);
 
+$OrderID = $_POST["OrderID"];
+$OrderTotal = $_POST["OrderTotal"];
+$InvoiceTotal = $_POST["InvoiceTotal"];
+
 // Check connection
 if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "insert into order 
-        values (...);";
+$sql = "update orders
+	set OrderTotal=$OrderTotal, InvoiceTotal=$InvoiceTotal, OrderDate=CurDate(), OrderStatus='Pending' 
+        where OrderID=$OrderID;";
 echo $sql;
 
 // perform the query
-//$conn->query($sql);
+$conn->query($sql);
 
 //alert the user and send them to home page.
 session_start();
-$_SESSION['alert'] = "Checkout Complete! Kind of...";
+$_SESSION['alert'] = "Checkout Complete!";
 header('location:../index.php');
 
 ?>
